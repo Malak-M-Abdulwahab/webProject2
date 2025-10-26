7d521fc97720ce77912a59478cf795576ecabc94
Malakk <malak.m.abdelwahab05@gmail.com>
2025-10-26 23:21:43 +0300

    removed the stuff i used for uni

--- js/Register.js
+++ js/Register.js
@@ -2,7 +2,6 @@ var firstName = document.querySelector("#first-name")
 var lastName = document.querySelector("#last-name")
 var email = document.querySelector("#email")
 var password = document.querySelector("#password")
-var passwordCheck = document.querySelector("#password-check")
 var register = document.querySelector("#submit")
 
 register.addEventListener("click", function(e){
@@ -19,9 +18,6 @@ register.addEventListener("click", function(e){
     if(password){
         password.style.borderColor = "rgb(204, 204, 204)"
     }
-    if(passwordCheck){
-        passwordCheck.style.borderColor = "rgb(204, 204, 204)"
-    }
     if(!firstName.value || !lastName.value || !email.value || !password.value || !passwordCheck.value){
         if(!firstName.value){
            firstName.style.borderColor = "red"
@@ -35,9 +31,6 @@ register.addEventListener("click", function(e){
         if(!password.value){
             password.style.borderColor = "red"
         }
-        if(!passwordCheck.value){
-            passwordCheck.style.borderColor = "red"
-        }
         alert("All inputs are required, please fill them all.")
         return
     }
@@ -48,32 +41,4 @@ register.addEventListener("click", function(e){
         alert("Enter correct email.")
         return
     }
-    if(password.value != passwordCheck.value){
-        alert("Passwords are not the same.")
-        return
-    }
-
-    var fullName = firstName.value.trim() + " " + lastName.value.trim()
-
-    var formData = new FormData()
-
-    formData.append("name", fullName)
-    formData.append("email", email.value)
-    formData.append("password", password.value)
-
-    fetch("register.php", { // remember to also remove all this when working on your website
-        method: "POST",
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        if(data.includes("success")){
-            setTimeout(() => {
-        window.location = "Login.html"
-        }, 1000);
-        }
-    })
-    .catch(error =>{
-        alert("Error fetching data: ", error)
-    })
 })
